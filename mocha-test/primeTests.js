a40955542906718f63c7fbcb0dca1bf970fe59ac
guilddrive3 <guilddrive3@gmail.com>
2021-08-31 22:41:53 -0500

    testing on mac

---
+++ mocha-test/primeTests.js
@@ -0,0 +1,20 @@
+//suppose have many functions to test and want tests in separate file
+"use strict";
+const assert = require("assert");
+/* import from functions.js module */
+const functionsModule = require("./prime.js");
+const checkPrime = functionsModule.checkPrime;
+const ssReverse = functionsModule.ssReverse;
+describe("checkPrime returns true if argument is prime ", function () {
+    it("37 is prime", function () {
+        assert.strictEqual(checkPrime(37), true);
+    });
+    it(" 77 is not prime", function () {
+        assert.strictEqual(checkPrime(77), false);
+  });
+});
+describe("ssReverse", function () {
+    it("[1, 2, 3, 4]", function () {//test array equality
+        assert.deepStrictEqual(ssReverse([1, 2, 3, 4]), [4, 3, 2, 1]);
+    });
+});
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log('hi world');
+console.log("hi world");
+
+/**
+ * 
+ * @param {number} bar is any number
+ * @returns {number} the foo of bar
+ */
+function foo(bar){
+
+}
